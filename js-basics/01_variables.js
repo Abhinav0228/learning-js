bcce1d4afb70d5494d3a4ef525ce593b593d52c8
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-10-29 13:06:13 +0000

    new changes

---
+++ js-basics/01_variables.js
@@ -0,0 +1,13 @@
+const accId = 20;
+let accEmail = "@mail.com"
+var accPass = "Abhi"
+City = "chd"
+let accState;
+// accId = 2;
+
+// console.log(accId);   // not allowed
+accEmail = "abb@mail.com"
+accPass = "aaaa"
+City = "pun"
+
+console.table([accId, accEmail, accPass, City, accState])
--- js-basics/js1.js
+++
@@ -1,4 +0,0 @@
-console.log("hello");
-
-let name1 = "Abhinav";
-console.log(name1);
