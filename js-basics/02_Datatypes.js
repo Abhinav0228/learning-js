6788265d2a40da2ded756d046efb7e8ae903fed1
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-10-29 17:29:13 +0000

    datatypes

---
+++ js-basics/02_Datatypes.js
@@ -0,0 +1,12 @@
+let name = "abhinav"  //string
+let age = 24  //number
+let isLoogedIn = false  //boolean
+
+//Bigint
+//null
+//undefined
+//symbol => unique
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
