448ef3e5f372635a7de9718da921b9bc69da009d
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-11-04 08:32:21 +0000

    datatypes value

--- js-basics/02_Datatypes.js
+++ js-basics/02_Datatypes.js
@@ -10,3 +10,10 @@ let isLoogedIn = false  //boolean
 
 console.log(typeof undefined);  //undefined
 console.log(typeof null);  //object
+
+// Two datatypes:
+// 1. Primitive => call by value   =>7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// 2. Non-primitive => Reference type  => Arrays, Objects, Functions
+
+
+// Javascript is a dynamic typing language. When you declare a variable, you do not need to specify what type this variable is.
