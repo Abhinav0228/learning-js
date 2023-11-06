4b2daa08a6c42892ed730432cc21281c48a8ba8d
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-11-06 08:32:00 +0000

    String

---
+++ js-basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "Abhinav"
+const repoCount = "20"
+
+// console.log(name + repoCount);
+
+// console.log(`My name is ${name} and my repo Count is ${repoCount}`);
+// console.log(name[0]);
+
+// another way to declare string 
+// string is an object and have multiple properties
+
+let gameName = new String('mygamee')  
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('g'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
