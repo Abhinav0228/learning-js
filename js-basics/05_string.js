8e645c494de36da33a4c09b81d0c082470c569c6
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-11-13 12:35:46 +0000

    string properties

--- js-basics/05_string.js
+++ js-basics/05_string.js
@@ -11,10 +11,31 @@ const repoCount = "20"
 
 let gameName = new String('mygamee')  
 // console.log(gameName[0]);
-// console.log(gameName.length);
+// console.log(gameName.length)
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('g'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)  // does not include 4, only 0-1-2-3 not 4
+// console.log(newString);
+
+const anotherString = gameName.slice(-5, 4)   
+// console.log(anotherString);
+
+const trimString = "    Assd             "
+// console.log(trimString);
+// console.log(trimString.trim());
+
+const url = "https://gaming.com/my%20game"
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('game'));
+
+let splitStr = "my name is abhinav sharma"
+// console.log(splitStr.split(" "));
+
+const newSplit = splitStr.split(" ")
+// console.log(newSplit[2]);
+
+console.log(splitStr.slice('0', '10'));
+
