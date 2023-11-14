4653c0cd07abfc0ad61b963f5a60bbbd0b3868f6
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-11-14 08:17:10 +0000

    number and maths

---
+++ js-basics/06_Nums_and_Maths.js
@@ -0,0 +1,28 @@
+const num = 100;
+// console.log(num);
+
+const balance = new Number(400)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 123.985;
+// console.log(otherNum.toPrecision(5));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++++++  Maths   +++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.4));
+// console.log(Math.min(4, 7, 9, 5));
+// console.log(Math.max(4, 7, 9, 5));
+
+console.log(Math.random());
+
+
+
