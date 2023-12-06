d85d539cac3f965352866ea8e51f54bd1849d9af
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-12-06 13:27:19 +0000

    dates n time

--- js-basics/07_date_n_time.js
+++ js-basics/07_date_n_time.js
@@ -1,2 +1,25 @@
 let myDate = new Date()
-console.log(myDate);
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myNewDate = new Date(2023, 0, 23)
+// console.log( myNewDate.toDateString());
+
+// let myNewDate = new Date(2023, 0, 23, 5, 3)
+// console.log( myNewDate.toLocaleString());
+
+// let myTimeStemp = Date.now();
+// let myTimeStemp = (Math.floor(Date.now()/1000))
+// console.log(myTimeStemp);
+
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1);
+let neww = newDate.toLocaleString('default', {
+    weekday: "long"
+})
+
+console.log(neww);
