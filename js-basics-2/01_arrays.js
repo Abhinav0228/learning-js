824b4be418cfb01dc8dcb9e05d580d46a7083a79
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-12-07 13:33:10 +0000

    arrays

---
+++ js-basics-2/01_arrays.js
@@ -0,0 +1,21 @@
+// enables storing a collection of multiple items under a single variable name
+
+// const myArr = [0 ,5, 7, 3]
+// console.log(myArr[2]);
+
+// Array methods
+// myArr.push(4)
+// myArr.pop()
+// myArr.unshift(6)  add value on the first index(0)
+// myArr.shift()
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(3));
+
+// const anotherArr = myArr.join()
+// console.log(myArr);
+// console.log(anotherArr);     //will convert array to string
+// console.log(typeof anotherArr);
+
+// slice, splice 
+
