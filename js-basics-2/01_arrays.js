9bd2777b7a74a95ee68daf3a011db8db4ee336a5
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2023-12-11 03:15:37 +0000

    arrayss

--- js-basics-2/01_arrays.js
+++ js-basics-2/01_arrays.js
@@ -1,11 +1,12 @@
 // enables storing a collection of multiple items under a single variable name
 
-// const myArr = [0 ,5, 7, 3]
+const myArr = [0 ,5, 7, 3]
 // console.log(myArr[2]);
 
 // Array methods
 // myArr.push(4)
 // myArr.pop()
+// console.log(myArr);
 // myArr.unshift(6)  add value on the first index(0)
 // myArr.shift()
 
@@ -19,3 +20,31 @@
 
 // slice, splice 
 
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+// console.log("C ", myArr);
+
+
+
+const marvel_heros = ["iron_man", "spiderman", "thor"]
+const dc_heros = ["batman", "superman", "flash"]
+
+// marvel_heros.push(dc_heros);   //this array will not merge together, it will add dc_heros as 4th element in the marvel_heros array
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);   //flash
+
+// concat operation
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+
+// spread operator - all elements get individual value 
+// think of like you had a glass cup and you dropped it
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
