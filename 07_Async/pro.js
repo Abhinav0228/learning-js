72734eaa1165298e34b7abb159d371f3ae79bee1
Abhinav Sharma <sharmaabhinav0228@gmail.com>
2024-02-15 21:56:33 +0530

    set interval

---
+++ 07_Async/pro.js
@@ -0,0 +1,10 @@
+// Random color generator
+
+const randomColor = function() {
+    const hex = "0123456ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16 )]; 
+    }
+    return color
+};
