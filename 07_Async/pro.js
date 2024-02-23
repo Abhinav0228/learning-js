60319739e688dc97d268a2e9360dce824249bc7b
Abhinav Sharma <sharmaabhinav0228@gmail.com>
2024-02-24 01:01:57 +0530

    project

--- 07_Async/pro.js
+++ 07_Async/pro.js
@@ -1,10 +1,28 @@
 // Random color generator
 
 const randomColor = function() {
-    const hex = "0123456ABCDEF";
+    const hex = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16 )]; 
+        console.log(color);
     }
-    return color
+    return color;
 };
+let intervalId
+const startChangingColor = function(){
+   intervalId = setInterval(changeBgColor, 1000)
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor()
+    }
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+}
+
+document.querySelector('#start').addEventListener("click", startChangingColor)
+
+
+document.querySelector('#stop').addEventListener("click", stopChangingColor)
