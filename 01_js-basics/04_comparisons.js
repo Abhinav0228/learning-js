c832df4d803627eed91b2c4bd3a6ad12d231be38
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2024-08-07 12:36:34 +0530

    Update 04_comparisons.js

--- 01_js-basics/04_comparisons.js
+++ 01_js-basics/04_comparisons.js
@@ -19,3 +19,5 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);
+// The double equal operator(==) only checks if two objects are references to the same object in memory,
+// while the triple equal operator(===) checks if two objects have the same properties and values.
