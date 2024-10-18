849e81304bd2e48b946572399de8bee59cf51262
Abhinav0228 <49275076+Abhinav0228@users.noreply.github.com>
2024-10-18 18:59:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ let totalFirst = document.getElementById('totalFirst')
 let calcFirst = document.getElementById('calcFirst')
 let calcSecond = document.getElementById('calcSecond')
 let calcThird = document.getElementById('calcThird')
+let grandBtn = document.querySelectorAll('.finalTotalBtn')
+let grandMain = document.querySelectorAll('.grandMain')
+let grandMarksShow = document.querySelector('.grandMarksShow')
+
 
 first.forEach((input, index) => {
     input.addEventListener('change', () => {
         let num = parseFloat(input.value);
         let status = document.querySelectorAll('.status1')[index];
-
         if (!isNaN(num)) {
+            // arr.push(num)
             if (num >= 33) {
-                console.log('pass');
+                // console.log('pass');
                 status.innerHTML = "Pass";
             } else {
                 console.log('fail');
@@ -34,38 +38,42 @@ first.forEach((input, index) => {
 calcFirst.addEventListener('click', () => {
     const totalSum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     totalFirstDisplay.textContent = `Total: ${totalSum}`; 
-    console.log(`Total: ${totalSum}`); 
-
+    console.log(`Total: ${totalSum}`);  
+    
     const percentOne = (totalSum / 600 ) * 100 ;
     const statusFirst = document.getElementById('statusFirst')
     
     if ( percentOne >= 33 && percentOne <= 50 ) {
         statusFirst.innerHTML = "Third"
-        statusFirst.style.color = "red"
+        statusFirst.style.backgroundColor = "#ff9033"
+        statusFirst.style.color = "#fff"
     }
     else if ( percentOne >=51 && percentOne <=60 ) {
         statusFirst.innerHTML = "Second"
-        statusFirst.style.color = "yellow"
+        statusFirst.style.backgroundColor = "#f6f69c"
+        statusFirst.style.color = "#000"
     }
     else if ( percentOne >=61 && percentOne <=80 ) {
         statusFirst.innerHTML = "First"
+        statusFirst.style.backgroundColor = "#d4fbffeb"
         statusFirst.style.color = "blue"
     }
     else if ( percentOne >=81 && percentOne <=90 ) {
         statusFirst.innerHTML = "Distinct"
-        statusFirst.style.color = "green"
+        statusFirst.style.backgroundColor = "#6bff6b"
+        statusFirst.style.color = "#000"
     }
     else if ( percentOne >=91 && percentOne <=100 ) {
         statusFirst.innerHTML = "Brilliant"
-        statusFirst.style.color = "purple"
+        statusFirst.style.background = "linear-gradient(25deg, #3eb489, #90EE90)";
+        statusFirst.style.color = "#000"
     }
     else {
         statusFirst.innerHTML = "Fail"
-        statusFirst.style.color = "gray"
+        statusFirst.style.backgroundColor = "#e00c0cc7"
+        statusFirst.style.color = "#fff"
     }
 });
-
-
 
 second.forEach((input, index) => {
     input.addEventListener('change', () => {
@@ -74,13 +82,12 @@ second.forEach((input, index) => {
 
         if (!isNaN(num)) {
             if (num >= 33) {
-                console.log('pass');
+                // console.log('pass');
                 status.innerHTML = "Pass";
             } else {
                 console.log('fail');
                 status.innerHTML = "Fail";
             }
-
             arrSecond[index] = num;
         } else {
             console.log("Invalid input");
@@ -99,27 +106,33 @@ calcSecond.addEventListener('click', () => {
     
     if ( percentSecond >= 33 && percentSecond <= 50 ) {
         statusSecond.innerHTML = "Third"
-        statusSecond.style.color = "red"
+        statusSecond.style.backgroundColor = "#ff9033"
+        statusSecond.style.color = "#fff"
     }
     else if ( percentSecond >=51 && percentSecond <=60 ) {
         statusSecond.innerHTML = "Second"
-        statusSecond.style.color = "yellow"
+        statusSecond.style.backgroundColor = "#f6f69c"
+        statusSecond.style.color = "#000"
     }
     else if ( percentSecond >=61 && percentSecond <=80 ) {
         statusSecond.innerHTML = "First"
+        statusSecond.style.backgroundColor = "#d4fbffeb"
         statusSecond.style.color = "blue"
     }
     else if ( percentSecond >=81 && percentSecond <=90 ) {
         statusSecond.innerHTML = "Distinct"
-        statusSecond.style.color = "green"
+        statusSecond.style.backgroundColor = "#6bff6b"
+        statusSecond.style.color = "#000"
     }
     else if ( percentSecond >=91 && percentSecond <=100 ) {
         statusSecond.innerHTML = "Brilliant"
-        statusSecond.style.color = "purple"
+        statusSecond.style.background = "linear-gradient(25deg, #3eb489, #90EE90)";
+        statusSecond.style.color = "#000"
     }
     else {
         statusSecond.innerHTML = "Fail"
-        statusSecond.style.color = "gray"
+        statusSecond.style.backgroundColor = "#e00c0cc7"
+        statusSecond.style.color = "#fff"
     }
 });
 
@@ -131,7 +144,7 @@ third.forEach((input, index) => {
 
         if (!isNaN(num)) {
             if (num >= 33) {
-                console.log('pass');
+                // console.log('pass');
                 status.innerHTML = "Pass";
             } else {
                 console.log('fail');
@@ -156,26 +169,131 @@ calcThird.addEventListener('click', () => {
     
     if ( percentThree >= 33 && percentThree <= 50 ) {
         statusThird.innerHTML = "Third"
-        statusThird.style.color = "red"
+        statusThird.style.backgroundColor = "#ff9033"
+        statusThird.style.color = "#000"
     }
     else if ( percentThree >=51 && percentThree <=60 ) {
         statusThird.innerHTML = "Second"
-        statusThird.style.color = "yellow"
+        statusThird.style.backgroundColor = "#f6f69c"
+        statusThird.style.color = "#000"
     }
     else if ( percentThree >=61 && percentThree <=80 ) {
         statusThird.innerHTML = "First"
+        statusThird.style.backgroundColor = "#d4fbffeb"
         statusThird.style.color = "blue"
     }
     else if ( percentThree >=81 && percentThree <=90 ) {
         statusThird.innerHTML = "Distinct"
-        statusThird.style.color = "green"
+        statusThird.style.backgroundColor = "#6bff6b"
+        statusThird.style.color = "#000"
     }
     else if ( percentThree >=91 && percentThree <=100 ) {
         statusThird.innerHTML = "Brilliant"
-        statusThird.style.color = "purple"
+        statusThird.style.background = "linear-gradient(25deg, #3eb489, #90EE90)";
+        statusThird.style.color = "#000"
     }
     else {
         statusThird.innerHTML = "Fail"
-        statusThird.style.color = "gray"
+        statusThird.style.backgroundColor = "#e00c0cc7"
+        statusThird.style.color = "#fff"
     }
 });
+
+// let finalResult = []
+
+// for ( let i =0; i < arr.length; i++ ) {
+//     finalResult[i] = arr[i] + arrSecond + arrThird
+// }
+
+// console.log(finalResult);
+
+
+
+
+
+// let arrGrand = [];
+
+// grandMain.forEach((item, index) => {
+//     arrGrand[index] = []; 
+// });
+
+// console.log(arrGrand);
+
+// grandBtn.forEach((item) => {
+//     item.addEventListener ('click', ()=> {
+                
+        
+//         for ( let i =0; i < arr.length; i++  ) {
+//             let finalResult = [i]
+//             finalResult[i] = arr[i] + arrSecond[i] + arrThird[i]
+           
+
+//             grandMarksShow.innerHTML = finalResult[i]  
+//         }
+    
+//     })
+// })
+
+
+grandBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        let finalResult = []; 
+
+        for (let i = 0; i < arr.length; i++) {
+            finalResult[i] = (arr[i] || 0) + (arrSecond[i] || 0) + (arrThird[i] || 0);
+        }
+
+        document.getElementById(`showData${index + 1}`).innerHTML=finalResult[index]
+        // console.log(`Total Marks for Subject ${index + 1}:`, finalResult[index]);
+    });
+});
+
+
+
+
+
+// grandBtn.forEach((index) => {
+//     let btnTotal = index
+//     btnTotal.addEventListener ('click', ()=> {
+            
+    
+//         for ( let i =0; i < arr.length; i++  ) {
+//             let finalResult = []
+//             finalResult = arr[i] + arrSecond[i] + arrThird[i]
+//             console.log(finalResult);
+//         }
+       
+    
+//     })
+
+// })
+
+
+
+// grandBtn.forEach((item, index) => {
+//     let newArr = []
+//     item.addEventListener ('click', ()=> {
+//         for (let i = 0; i <= index; i++){
+//             newArr += index
+//             console.log(newArr, index);
+            
+
+//         }
+        
+//         let arr = []
+        
+//     })
+// })
+
+
+
+
+
+
+
+let finalBtn = document.querySelectorAll('.finalTotalBtn') 
+finalBtn.forEach ((element, index) => {
+    element.setAttribute('id', 'grandBtn-' + index )
+    
+})
+
